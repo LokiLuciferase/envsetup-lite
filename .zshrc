ed30a15b040abc08905a9fc073a9d12ca87c3135
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2020-04-08 11:16:38 +0200

    initial commit

---
+++ .zshrc
@@ -0,0 +1,20 @@
+unsetopt nomatch
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+COMPLETION_WAITING_DOTS="true"
+HIST_STAMPS="dd.mm.yyyy"
+DEFAULT_USER=$USER
+EDITOR=/bin/nano
+plugins=(git)
+[[ -d "$ZSH/custom/plugins/zsh-syntax-highlighting" ]] && plugins+=(zsh-syntax-highlighting)
+source $ZSH/oh-my-zsh.sh
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$HOME/bin:$HOME/.local/bin:/usr/games
+export DISPLAY=localhost:0.0
+
+[[ ! -f "$HOME/.dircolors" ]] || eval "$(dircolors -b "$HOME/.dircolors" )" && zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+for config in ".zsh_aliases" ".zsh_ssh_aliases" ".zsh_functions" ".p10k.zsh"; do
+    [[ ! -f "$HOME/$config" ]] || source "$HOME/$config"
+done
+
+cd
---
+++ set_up.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+DO_PYTHON=false
+DO_ENV=false
+
+if [[ "$#" -eq 0 ]]; then
+    DO_PYTHON=true
+    DO_ENV=true
+elif [[ "$1" = "python" ]]; then
+    DO_PYTHON=true
+elif [[ "$1" = "env" ]]; then
+    DO_ENV=true
+else
+    echo "Supplied command line argument(s) [ $@ ] are invalid."
+    exit 1
+
+[[ -z "$(which wget)" ]] && echo "wget not installed. Exiting..." && exit 1
+
+if [[ "$DO_PYTHON" = true ]]; then
+    # install anaconda3
+    mkdir -p anaconda_install && cd anaconda_install
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+    cd .. && rm -rf anaconda_install
+    export PATH=$HOME/miniconda3/bin:$PATH
+    conda env update -f essentials.yaml
+    conda clean -a --yes
+
+    # setup setup custom jupyter stuff and jupyterthemes
+    jupyter contrib nbextension install --sys-prefix
+    jupyter nbextension enable rise --py --sys-prefix
+    jupyter nbextension enable comment-uncomment/main
+    jupyter nbextension enable execute_time/ExecuteTime
+    jupyter nbextension enable scroll_down/main
+    jupyter nbextension enable code_prettify/autopep8
+    jupyter nbextension enable varInspector/main
+    jupyter nbextension enable rubberband/main
+    jt -t oceans16 -tfs 14 -ofs 10 -f dejavu -cellw 95% -altmd -T
+fi
+
+if [[ "$DO_ENV" = true ]]; then
+    [[ -z "$(which zsh)"  ]] && echo "ZSH not installed. Exiting..." && exit 1
+    [[ -z "$(which git)"  ]] && echo "git not installed. Exiting..." && exit 1
+    # set up environment and shell
+    mkdir -p $HOME/.ssh && cp .ssh/config $HOME/.ssh
+    export CHSH=no
+    sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+    cp .p10k.zsh .zsh_aliases .zshrc $HOME
+    [[ "$DO_PYTHON" = true ]] && conda init bash zsh
+fi
