01803d85074aacc60127cfd99a64ce43bb7462f1
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2020-06-26 17:22:44 +0200

    declare all functions to root

--- set_up.sh
+++ set_up.sh
@@ -4,8 +4,8 @@ source functions.sh
 
 # run selected
 # run stuff requiring that sudo be called
-[[ "$ALLOW_SUDO" = true ]] && sudo bash -c "$(declare -f do_minimal_f); do_minimal_f"
-[[ "$ALLOW_SUDO" = true ]] && [[ "$DO_EXTRAS" = true ]] && sudo bash -c "$(declare -f do_extras_f); do_extras_f"
+[[ "$ALLOW_SUDO" = true ]] && sudo bash -c "$(declare -f); do_minimal_f"
+[[ "$ALLOW_SUDO" = true ]] && [[ "$DO_EXTRAS" = true ]] && sudo bash -c "$(declare -f); do_extras_f"
 # if we don't run with sudo and we have no wget, we can't install python or zsh
 [[ -z "$(which wget)" ]] && echo "wget not installed. Exiting..." && exit 1
 [[ "$DO_PYTHON" = true ]] && do_python_f
