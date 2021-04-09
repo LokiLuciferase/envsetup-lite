26d16579421993a40bf0efead8102b2162bf46dd
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2021-04-09 12:25:40 +0200

    update dotfiles and add zsh autosuggestions

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -75,6 +75,7 @@ function do_env_f {
     export RUNZSH=no
     sh -c "$(curl -sL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
     # install dotfiles: intended to be easily updated by pulling
